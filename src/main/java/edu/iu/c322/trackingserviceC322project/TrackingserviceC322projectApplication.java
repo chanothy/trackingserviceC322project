183d698735585b77ac46b3a4d69c5b3e80ce0452
timchan <timchan@iu.edu>
2023-04-05 19:25:48 -0400

    Initial commit

---
+++ src/main/java/edu/iu/c322/trackingserviceC322project/TrackingserviceC322projectApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.trackingserviceC322project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingserviceC322projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingserviceC322projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/c322/trackingserviceC322project/TrackingserviceC322projectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.trackingserviceC322project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingserviceC322projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
